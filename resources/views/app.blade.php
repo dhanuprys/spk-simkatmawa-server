dbeac327130152f2d4f41c331f0a619ce08dfc24
Gede Dhanu Purnayasa <dhanuprys@gmail.com>
2025-07-22 12:42:48 +0800

    feat: add google analytics on application frontpage (app.blade.php)

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,6 +1,15 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}" @class(['dark' => ($appearance ?? 'system') == 'dark'])>
     <head>
+        <!-- Google tag (gtag.js) -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-3YF64E74RY"></script>
+        <script>
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+        
+          gtag('config', 'G-3YF64E74RY');
+        </script>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
